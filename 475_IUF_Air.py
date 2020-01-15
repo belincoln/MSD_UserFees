79816e0787daebca7eedd695869448ec875cf1b5
Bennett Lincoln <belincoln@deloitte.com>
2020-01-15 13:31:13 -0500

    Initial Commit, python files

---
+++ 475_IUF_Air.py
@@ -0,0 +1,247 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 16 13:05:35 2019
+
+@author: belincoln
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+#%% Load Data
+
+# Remember to set the working directory to whatever folder you have saved the zip file to. 
+# Works well for Jupyter Notebooks, can be configured in IDE using file explorer. 
+collections_475 = pd.read_excel(os.path.join('Source Emails','Files','Collections','IUF_Air','Collections cc475 - FY13 - FY18.xls'))
+
+#%% Clean Data
+
+
+# delete columns and rows that contain only na
+collections_475 = collections_475.dropna(axis=0, how = 'all')
+collections_475 = collections_475.dropna(axis=1, how = 'all')
+
+# delete first two rows
+collections_475 = collections_475.iloc[2:,:]
+#%%
+#Fill down the company column
+collections_475.iloc[:,0] = collections_475.iloc[:,0].ffill(axis = 0)
+# reset index
+collections_475 = collections_475.reset_index(drop=True)
+
+#%% drop columns
+collections_475 = collections_475.iloc[:,[0,1,7,8,9,11]]
+
+# Give column headers
+#%%
+collections_475.columns = collections_475.iloc[0]
+
+#rename columns
+collections_475.columns.values[0] = 'Company'
+collections_475.columns.values[1] = 'Period'
+collections_475.columns.values[2] = 'Class Code'
+
+# delete first two rows
+collections_475 = collections_475.iloc[2:,:]
+#%%
+# Remove excess rows 
+collections_475 = collections_475.dropna(subset = ['Class Code'])
+# Validate that data file contains only one conveyance, use conveyance to label total collections column
+column_name = collections_475['Class Code'].unique()[0]
+# Create total collections column
+collections_475[column_name] = collections_475.iloc[:,-3:].sum(axis = 1)
+#delete excess columns
+collections_475 = collections_475.loc[:,[column_name,'Period']]
+
+#%%
+# Sum on Remittance Period
+collections_475 = collections_475.groupby(collections_475['Period']).sum()
+# Remove audit payments
+collections_475 = collections_475[~collections_475.index.str.contains("\*")]
+# remove payments outside of 2013-2018
+
+# Add an additional column that shows remittance period (independent of year)
+collections_475['Remittance Period'] = collections_475.index.str.split('20').str[0]
+
+
+# Create Calendar Year Column
+collections_475['Calendar Year'] = collections_475.index.str.split(')').str[1]
+# Turn Years into integers
+collections_475['Calendar Year'] = collections_475['Calendar Year'].astype(int)
+
+# Filter on years not a part of analysis
+years = [2012,2013,2014,2015,2016,2017,2018]
+collections_475 = collections_475[collections_475['Calendar Year'].isin(years)]
+
+# rename collections column
+collections_475.rename(columns = {'475 - Air Pass (Immigration)':'Collections'}, inplace = True)
+
+#%%
+
+workload = pd.read_excel(os.path.join('Source Emails','Files','Workload','IUF_Air','FY09-fy18-passenger data air and cruise.xlsx'))
+
+#Clean Workload Df
+#remove cruise data
+workload = workload.iloc[0:6,:]
+#select data we want to work with
+workload2 = workload.iloc[::2,:2]
+workload3 = workload.iloc[[1,3,5],2:]
+#reset indices for merge
+workload2.reset_index(drop = True, inplace = True)
+workload3.reset_index(drop = True, inplace = True)
+
+# concat dataframes to get cleaned workload df
+workload = pd.concat([workload2, workload3], axis=1, sort=False)
+#sum Data Element and Workload ID columns into one descriptor
+workload['Workload Element'] = workload.iloc[:,0]+': '+workload.iloc[:,1]
+workload = workload.iloc[:,::-1]
+workload = workload.iloc[:,:-2]
+#%%
+workload = workload.transpose()
+#promote first row to column headers and drop first row
+workload.columns = workload.iloc[0]
+workload = workload.iloc[1:]
+
+# Create Calendar Year and Month Columns
+workload['Month'] = workload.index.str.split('/').str[0]
+workload['Calendar Year'] = workload.index.str.split('/').str[2] + " "
+print(workload['Calendar Year'])
+# Filter on years not a part of analysis
+years = ['2012 ','2013 ','2014 ','2015 ','2016 ','2017 ','2018 ']
+workload = workload[workload['Calendar Year'].isin(years)]
+
+#%%
+# Build out Remittance Period Columns
+conditions = [(workload['Month'] == '1'), (workload['Month'] == '2'), (workload['Month'] == '3'), (workload['Month'] == '4'), (workload['Month'] == '5'), (workload['Month'] == '6'),(workload['Month'] == '7'),(workload['Month'] == '8'),(workload['Month'] == '9'),(workload['Month'] == '10'),(workload['Month'] == '11'),(workload['Month'] == '12')] 
+choices = ['Qtr 01 (Jan-Mar)','Qtr 01 (Jan-Mar)','Qtr 01 (Jan-Mar)','Qtr 02 (Apr-Jun)','Qtr 02 (Apr-Jun)','Qtr 02 (Apr-Jun)','Qtr 03a (Jul-Aug)','Qtr 03a (Jul-Aug)', 'Qtr 03b (Sept)','Qtr 04 (Oct-Dec)','Qtr 04 (Oct-Dec)','Qtr 04 (Oct-Dec)']
+workload['Period'] = np.select(conditions, choices, default='error')
+
+
+#%%
+
+# Match Period Column to Collections
+workload['Period'] = workload['Period'] + ' ' + workload['Calendar Year']
+# Set index to Remittance Period
+workload = workload.set_index('Period')
+# drop unnecssary columns
+workload = workload.drop(['Calendar Year','Month'], axis = 1)
+# Sum on Remittance Period
+workload = workload.groupby(workload.index).sum()
+
+
+
+#%%
+#remove non FY2013-2018 data
+workload = workload.iloc[1:,:]
+collections_475 = collections_475.iloc[1:-1,:]
+
+#%%
+#remove non FY2013-2018 data
+searchfor = ['Qtr 02 \(Apr-Jun\) 2012', 'Qtr 03a \(Jul-Aug\) 2012','Qtr 03b \(Sept\) 2012']
+collections_475 = collections_475[~collections_475.index.str.contains('|'.join(searchfor))]
+workload = workload[~workload.index.str.contains('|'.join(searchfor))]
+
+#Add sum of workload columns
+workload['Workload'] = workload.sum(axis = 1)
+
+#%%
+workload_collections = pd.merge(workload,collections_475,how = 'inner', left_index = True, right_index = True)
+#%%
+# create a sum of all relevant workload metrics
+#workload_collections['Workload'] = workload_collections.iloc[:,:3].sum(axis = 1)
+#test for correlation
+corr = workload_collections.iloc[:,:-1].corr()
+# Select Workload metric with highest correlation
+#workload_collections = workload_collections.iloc[:,2:-1]
+#%% Scatterplots of workload metrics and collection
+for i in range(len(workload.columns)):
+    plt.scatter(workload_collections.iloc[:,i],workload_collections['Collections'])
+    plt.xlabel(workload.columns[i])
+    plt.ylabel('Total Collections')
+    plt.show()
+
+#%%
+import statsmodels.api as sm
+
+# run linear regression on workload and collections to get linear coefficent. 
+X = workload_collections['Workload']
+X = sm.add_constant(X)
+model = sm.OLS(workload_collections['Collections'],X)
+results = model.fit()
+
+# this is the Regression coefficent
+collection_per_workload = results.params.iloc[1]
+
+# Multiply linear coefficent with workload to graph a line of best fit. 
+workload_collections['Expected Collections'] = collection_per_workload * workload_collections['Workload']
+# Add regression coefficent and R^2 values for notecards
+workload_collections['Regression Coefficent'] = results.params.iloc[1]
+workload_collections['R^2'] = results.rsquared
+workload_collections = workload_collections.iloc[:,3:]
+
+#%%
+
+# Show final Scatter Plot with Line of best fit. 
+plt.scatter(workload_collections['Workload'], workload_collections['Collections'])
+plt.xlabel('Workload')
+plt.ylabel('Collections')
+plt.plot(workload_collections['Workload'], results.params.iloc[0] + collection_per_workload * workload_collections['Workload'])
+plt.show()
+
+#%%
+#Add Power BI column for filter
+workload_collections['Fee'] = 'IUF'
+workload_collections['Environment'] = 'Air'
+
+# add coefficents and constants for confidence interval mapping
+workload_collections['lower conf_int constant'] = results.conf_int(alpha=.05, cols= None).iloc[0,0]
+workload_collections['lower conf_int coefficent'] = results.conf_int(alpha=.05, cols= None).iloc[1,0]
+
+workload_collections['upper conf_int constant'] = results.conf_int(alpha=.05, cols= None).iloc[0,1]
+workload_collections['upper conf_int coefficent'] = results.conf_int(alpha=.05, cols= None).iloc[1,1]
+
+#Write csv file for power bi
+workload_collections.to_csv(os.path.join('Power_BI_Data_Files','475_IUF_Air_Workload_Collections_Period.csv'))
+
+#%%
+
+# =============================================================================
+# from scipy.stats import t
+# 
+# #Set confidence interval
+# confidence = 0.95
+# data = workload_collections[['Collections','Expected Collections','Workload']]
+# data['squared_diff'] = (data['Collections'] - data['Expected Collections']) ** 2
+# temp = np.sum(data['squared_diff'])
+# temp = temp / (len(data)-2)
+# se = temp ** .5
+# #%%
+# 
+# denom = np.sum(data['Collections']**2)
+# denom = denom - (np.sum(data['Collections'])**2 / len(data))
+# num = (data['Workload'] - np.mean(data['Workload'])) ** 2
+# temp = 1/len(data) + num/denom
+# temp = temp ** .5
+# 
+# data['Upper Confidence Interval'] = data['Expected Collections'] + t.ppf(1-(1-confidence)/2,(len(data)-2))*se*temp
+# data['Lower Confidence Interval'] = data['Expected Collections'] - t.ppf(1-(1-confidence)/2,(len(data)-2))*se*temp
+# 
+# 
+# #%%
+# #plt.scatter(workload_collections['Workload'],data['Collections'])
+# plt.plot(workload_collections['Workload'],data['Expected Collections'])
+# plt.scatter(workload_collections['Workload'],data['Collections'])
+# plt.plot(workload_collections['Workload'],data['Upper Confidence Interval'])
+# plt.plot(workload_collections['Workload'],data['Lower Confidence Interval'])
+# plt.xlabel('Workload')
+# plt.ylabel('Total Collections')
+# plt.show()
+# 
+# =============================================================================
+#%%
+
+
+
+
---
+++ AQI_Rail.py
@@ -0,0 +1,161 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  8 15:03:34 2020
+
+@author: belincoln
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+
+#%% Load Data
+
+# Remember to set the working directory to whatever folder you have saved the zip file to. 
+# Works well for Jupyter Notebooks, can be configured in IDE using file explorer. 
+data = pd.read_excel(os.path.join('Source Emails','Files','Collections','AQI','AQI Collections.xlsx'),'Rail')
+#%%
+
+Rail_Collections = data[['Fiscal year', 'Fiscal period','Document Date', '$']]
+Rail_Collections = Rail_Collections.rename(columns={'$': 'Collections'})
+# All collection totlas came in as negative?
+Rail_Collections['Collections'] = (Rail_Collections['Collections'] * -1).astype(float)
+
+#%%
+
+# Sum on Remittance Period
+Rail_Collections = Rail_Collections.groupby(['Fiscal year','Fiscal period']).sum()
+
+#%%
+
+# Convert Fiscal Year and Month to Calendar Year and Month
+fy = []
+month = []
+for i in Rail_Collections.index:
+    fy.append(i[0])
+    month.append(i[1])
+    
+Rail_Collections['Month'] = month 
+Rail_Collections['Month'] = Rail_Collections['Month'] + 9
+Rail_Collections['Month'][Rail_Collections['Month'] >12] = Rail_Collections['Month'][Rail_Collections['Month'] >12] - 12
+
+Rail_Collections['Fiscal year'] = fy
+
+cy = Rail_Collections['Fiscal year'][Rail_Collections['Month'] > 9] -1
+cy = cy.append(Rail_Collections['Fiscal year'][Rail_Collections['Month'] < 10])
+Rail_Collections['Calendar Year'] = cy
+
+#%%
+Rail_Collections.set_index(Rail_Collections['Month'].apply(str) + "/1/" + Rail_Collections['Calendar Year'].apply(str), inplace = True)
+
+#%% Read and Clean Workload Data
+workload = pd.read_excel(os.path.join('Source Emails','Files','Workload','COBRA','fy13-18 stats by_Month National.xlsx'))
+# Select only rail rows
+workload = workload.iloc[2:4,:]
+#select data we want to work with
+workload.iloc[0,3:] = workload.iloc[1,3:]
+workload = workload.iloc[0,:]
+workload.drop(['Line','Total', 'Data Id', 'Data Elements  - National'],inplace = True)
+workload = workload.to_frame()
+workload = workload.astype(float)
+workload.rename(columns = {2:'Trains'}, inplace =True)
+
+
+#%%
+# merge workload and collections
+
+workload_collections = Rail_Collections.merge(workload,left_index = True,right_index = True)
+#%% Identify periods of fee rate change
+
+workload_collections['old fee rate'] = 1
+workload_collections.loc['1/1/2016':,'old fee rate'] = 0
+
+#%%
+# Shift collections to match with worklaod
+
+workload_collections['Collections_Shift'] = workload_collections['Collections'].shift(-3)
+workload_collections.drop(workload_collections.tail(3).index, inplace = True)
+
+#%% Show Scatter
+
+colors = np.where(workload_collections['old fee rate']==1,'b','r')
+plt.scatter(workload_collections['Trains'], workload_collections['Collections_Shift'], c=colors)
+plt.xlabel('Trains')
+plt.ylabel('Collections')
+plt.show()
+
+#%%
+
+# Perform R^2 Calculation to determine the % of change of collections that is explained by variation in workload
+import statsmodels.api as sm
+
+# set x and ys 
+r1_c_array = workload_collections['Collections_Shift'][workload_collections['old fee rate']==1]
+r2_c_array = workload_collections['Collections_Shift'][workload_collections['old fee rate']==0]
+
+r1_w_array = workload_collections['Trains'][workload_collections['old fee rate']==1]
+r2_w_array = workload_collections['Trains'][workload_collections['old fee rate']==0]
+
+# run linear regression on workload and collections to get linear coefficent. 
+X = r1_w_array.astype(float)
+X = sm.add_constant(X)
+model = sm.OLS(r1_c_array,X)
+results = model.fit()
+
+X2 = r2_w_array.astype(float)
+X2 = sm.add_constant(X2)
+model2 = sm.OLS(r2_c_array,X2)
+results2 = model2.fit()
+
+#%%
+workload_collections['Remittance Period'] = workload_collections['Fiscal year']
+workload_collections.drop(['Collections','Fiscal year', 'old fee rate','Month'], axis = 1, inplace = True)
+workload_collections = workload_collections.rename(columns={'Collections_Shift': 'Collections','Trains':'Workload'})
+workload_collections['Fee'] = 'AQI'
+
+#%%
+df1 = workload_collections.loc[:'1/1/2016',:]
+df1['Environment'] = 'Rail_Pre_Rate_Change'
+df1['R^2'] = results.rsquared
+df1['Regression Coefficent'] = results.params.iloc[1]
+df1['Expected Collections'] = results.params.iloc[0] + results.params.iloc[1] * df1['Workload']
+
+# add coefficents and constants for confidence interval mapping
+df1['lower conf_int constant'] = results.conf_int(alpha=.05, cols= None).iloc[0,0]
+df1['lower conf_int coefficent'] = results.conf_int(alpha=.05, cols= None).iloc[1,0]
+
+df1['upper conf_int constant'] = results.conf_int(alpha=.05, cols= None).iloc[0,1]
+df1['upper conf_int coefficent'] = results.conf_int(alpha=.05, cols= None).iloc[1,1]
+
+# Export as CSV for Power BI Visualization
+df1.to_csv(os.path.join('Power_BI_Data_Files','Pre_Rate_Change_AQI_Rail_Workload_Collections_Period.csv'))
+
+#%%
+#%%
+df2 = workload_collections.loc['1/1/2016':,:]
+df2['Environment'] = 'Rail_Post_Rate_Change'
+df2['R^2'] = results2.rsquared
+df2['Regression Coefficent'] = results2.params.iloc[1]
+df2['Expected Collections'] = results2.params.iloc[0] + results2.params.iloc[1] * df2['Workload']
+
+# add coefficents and constants for confidence interval mapping
+df2['lower conf_int constant'] = results2.conf_int(alpha=.05, cols= None).iloc[0,0]
+df2['lower conf_int coefficent'] = results2.conf_int(alpha=.05, cols= None).iloc[1,0]
+
+df2['upper conf_int constant'] = results2.conf_int(alpha=.05, cols= None).iloc[0,1]
+df2['upper conf_int coefficent'] = results2.conf_int(alpha=.05, cols= None).iloc[1,1]
+
+# Export as CSV for Power BI Visualization
+df2.to_csv(os.path.join('Power_BI_Data_Files','Post_Rate_Change_AQI_Rail_Workload_Collections_Period.csv'))
+# =============================================================================
+# # this is the linear coefficent
+# collection_per_workload = results.params.iloc[0]
+# # Multiply linear coefficent with workload to graph a line of best fit. 
+# workload_collections['Expected Collections'] = collection_per_workload * workload_collections['Total Workload']
+# # Add regression coefficent and R^2 values for notecards
+# workload_collections['Regression Coefficent'] = results.params.iloc[0]
+# workload_collections['R^2'] = results.rsquared
+# =============================================================================
+
